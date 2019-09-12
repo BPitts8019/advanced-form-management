5983086edbdec9b54087357b03fc1da2876e674e
BPitts8019 <bradleyj.pitts@gmail.com>
2019-09-11 18:19:18 -0700

    Added validation to the form

--- src/components/AnimalForm.js
+++ src/components/AnimalForm.js
@@ -1,17 +1,24 @@
 import React from 'react'
-import { withFormik, Form, Field } from "formik"
+import { withFormik, Form, Field } from "formik";
+import * as yup from "yup";
 
-const AnimalForm = (props) => {
+const AnimalForm = ({errors}) => {
    return (
       <Form>
+         {errors.species && <p className="error">{errors.species}</p>}
          <Field type="text" name="species" placeholder="Species" />
+         
+         {errors.age && <p className="error">{errors.age}</p>}
          <Field type="number" name="age" min="0" placeholder="Age" />
+         
+         {errors.diet && <p className="error">{errors.diet}</p>}
          <Field component="select" name="diet">
             <option value="" disabled>Select Diet:</option>
             <option value="carnivore">Carnivore</option>
             <option value="herbivore">Herbivore</option>
             <option value="omnivore">Omnivore</option>
          </Field>
+         {errors.vaccinations && <p className="error">{errors.vaccinations}</p>}
          <label>
             <Field type="checkbox" name="vaccinations" />
             <span>Vaccinations</span>
@@ -35,6 +42,12 @@ export default withFormik({
          notes: values.notes || ""
       };
    },
+   validationSchema: yup.object().shape({
+      species: yup.string().required(),
+      age: yup.number().positive().required(),
+      diet: yup.string().required(),
+      vaccinations: yup.boolean().oneOf([true])
+   }),
    handleSubmit: values => {
       console.log(values);
    }
