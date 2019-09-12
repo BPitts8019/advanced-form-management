9a890a341e0c2406d2afc634068c8ce4f83d13cc
BPitts8019 <bradleyj.pitts@gmail.com>
2019-09-11 20:31:07 -0700

    Nearly complete; need to add status from Formik to form data and POST request

--- src/components/AnimalForm.js
+++ src/components/AnimalForm.js
@@ -1,24 +1,25 @@
 import React from 'react'
 import { withFormik, Form, Field } from "formik";
 import * as yup from "yup";
+import axios from "axios";
 
-const AnimalForm = ({errors}) => {
+const AnimalForm = ({errors, touched}) => {
    return (
       <Form>
-         {errors.species && <p className="error">{errors.species}</p>}
+         {touched.species && errors.species && <p className="error">{errors.species}</p>}
          <Field type="text" name="species" placeholder="Species" />
          
-         {errors.age && <p className="error">{errors.age}</p>}
+         {touched.age && errors.age && <p className="error">{errors.age}</p>}
          <Field type="number" name="age" min="0" placeholder="Age" />
          
-         {errors.diet && <p className="error">{errors.diet}</p>}
+         {touched.diet && errors.diet && <p className="error">{errors.diet}</p>}
          <Field component="select" name="diet">
             <option value="" disabled>Select Diet:</option>
             <option value="carnivore">Carnivore</option>
             <option value="herbivore">Herbivore</option>
             <option value="omnivore">Omnivore</option>
          </Field>
-         {errors.vaccinations && <p className="error">{errors.vaccinations}</p>}
+         {touched.vaccinations && errors.vaccinations && <p className="error">{errors.vaccinations}</p>}
          <label>
             <Field type="checkbox" name="vaccinations" />
             <span>Vaccinations</span>
@@ -43,12 +44,18 @@ export default withFormik({
       };
    },
    validationSchema: yup.object().shape({
-      species: yup.string().required(),
-      age: yup.number().positive().required(),
-      diet: yup.string().required(),
-      vaccinations: yup.boolean().oneOf([true])
+      species: yup.string().required("Please enter a species for the animal."),
+      age: yup.number().positive().required("Umm.. The animal is how old?"),
+      diet: yup.string().required("Please select a diet for the animal."),
+      vaccinations: yup.boolean().oneOf([true], "Animal must be vaccinated!")
    }),
    handleSubmit: values => {
-      console.log(values);
+      axios.post("https://reqres.in/api/animals", values)
+         .then(response => {
+            console.log(response);
+         })
+         .catch(error => {
+            console.error(error);
+         })
    }
 })(AnimalForm);
